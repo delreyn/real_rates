a6e34ebc882b1c6148b63cfedc1a80ae29e10c3f
diego2017003 <30417399+diego2017003@users.noreply.github.com>
2021-11-28 20:55:52 -0300

    Update avaliacao_visualizacao.py

--- script/avaliacao_visualizacao.py
+++ script/avaliacao_visualizacao.py
@@ -57,49 +57,30 @@ def visualyze_presidents(fhc,lula,dilma_temer,bolso,moeda):
                        alpha=0.3)
         a_x.grid(alpha=0.5)
 
-    ### Ax1: FHC
     ax1.plot(fhc['Time'], fhc['rolling_mean'],
             color='#2A7E43')
-    ax1.set_xticklabels(['1999','',"","",'2001',"","",'','2003'],
+    ax1.set_xticklabels(['1999','','','','2001','','','','2003'],
                        alpha=0.3)
-    # ax1.text(700, 7.5, 'FHC', fontsize=18, weight='bold',
-    #         color='#2A7E43')
-    # ax1.text(730, 7, '(1995 - 2002)', weight='bold',
-    #         alpha=0.3)
 
-    ### Ax2: LULA
     ax2.plot(lula['Time'], lula['rolling_mean'],
             color='#ffa500')
     ax2.set_xticklabels(['', '2004', '', '', '', '2008', '',
                          '', '', '2011'],
                        alpha=0.3)
-    # ax2.text(700, 7.5, 'LULA', fontsize=18, weight='bold',
-    #         color='#ffa500')
-    # ax2.text(700, 7, '(2003 - 2010)', weight='bold',
-    #          alpha=0.3)
 
     ### Ax3: DILMA _ TEMER
     ax3.plot(dilma_temer['Time'], dilma_temer['rolling_mean'],
             color='#00B2EE')
-    ax3.set_xticklabels(["",'2011', '',"", '2014', '',
+    ax3.set_xticklabels(['','2011', '','', '2014', '',
                          '2016', '','', '2019'],
                        alpha=0.3)
-   # ax3.text(700.0, 7.5, 'DILMA-TEMER', fontsize=18, weight='bold',
-    #        color='#00B2EE')
-    #ax3.text(700,7, '(2011 - 2019)', weight='bold',
-     #       alpha=0.3)
+
     ### Ax4: BOZO
     ax4.plot(bolso['Time'], bolso['rolling_mean'],
             color='#FFD600')
     ax4.set_xticklabels(['2019',"","","","2020","","","", '2021'],
                        alpha=0.3)
-    # ax4.text(840, 7.5, 'BOLSONARO', fontsize=18, weight='bold',
-    #         color='#FFD600')
-    # ax4.text(150, 7, '(2019 - )', weight='bold',
-    #          alpha=0.3)
 
-
-    ### Ax4: Bush-Obama-Trump-Biden
     ax5.plot(fhc['Time'], fhc['rolling_mean'],
             color='#2A7E43')
     ax5.plot(lula['Time'], lula['rolling_mean'],
@@ -110,24 +91,31 @@ def visualyze_presidents(fhc,lula,dilma_temer,bolso,moeda):
             color='#FFD600')
     ax5.grid(alpha=0.5)
     ax5.set_xticks([])
-
-    ### Adding a title and a subtitle
-    # ax1.text(730, 10, 'BRL-'+moeda+' rate under 4 administrations',
-    #          fontsize=20, weight='bold')
-    # ax1.text(730, 8.5, '''BRL-'''+moeda+''' exchange rates under FHC (1995 - 2002),
-    #  Lula (2003 - 2010),
-    # and Dilma and Temer (2011-2018) e Bolsonaro (2019 - )''',
-    #         fontsize=16)
-
-    ### Adding a signature
-    # ax5.text(729, 0.65, '©DIEGO & REYNE' + ' '*188 + 'Source: European Central Bank',
-    #         color = '#f0f0f0', backgroundcolor = '#4d4d4d',
-    #         size=14)
-
     plt.savefig('chart'+moeda+'.png', bbox_inches="tight")
+def try_open_file(csv_file):
+    '''Esta funcao tenta abrir um arquivo csv e retorna o True or false
+    se o arquivo existir ou não respectivamente'''
+    try:
+        data = pd.read_csv(csv_file)
+        print(data.head())
+        return True
+    except IOError:
+        print("Arquivo não encontrado")
+        return False
+def convert_to_real(exchange):
+    '''converte da cotacao de euro->dolar e euro->real para
+    dolar->real'''
+    try:
+        exchange['real_dolar'] = exchange['BR_real']/exchange['US_dollar']
+        return exchange
+    except ZeroDivisionError:
+        print("Divisão por zero")
+        return None
+CSV_FILE = 'euro-daily-hist_1999_2020.csv'
 
+if try_open_file(CSV_FILE):
+    exchange_rates = pd.read_csv(CSV_FILE)
 
-exchange_rates = pd.read_csv('euro-daily-hist_1999_2020.csv')
 
 exchange_rates.rename(columns={'[US dollar ]': 'US_dollar',
                                'Period\\Unit:': 'Time',
@@ -141,18 +129,14 @@ exchange_rates['US_dollar'] = convert_column_to_float(exchange_rates,NAME_DOLAR)
 NAME_REAL = 'BR_real'
 exchange_rates['BR_real'] = convert_column_to_float(exchange_rates,NAME_REAL)
 
-exchange_rates['real_dolar'] = exchange_rates['BR_real']/exchange_rates['US_dollar']
+exchange_rates = convert_to_real(exchange_rates)
 
 real_euro = exchange_rates[['Time', 'BR_real']].copy()
 real_euro['rolling_mean'] = real_euro['BR_real'].rolling(30).mean()
 
 dolar_real = exchange_rates[['Time','real_dolar']]
 dolar_real['rolling_mean'] = dolar_real['real_dolar'].rolling(30).mean()
-#see_rolling_mean(dolar_real)
-# value1 = 'real_dolar'
-# #see_dataset(dolar_real,value1)
-# value2 = 'BR_real'
-#see_dataset(real_euro,value2)
+
 presidents_dolar = set_subdataset(dolar_real)
 fhc_dolar = presidents_dolar.copy(
        )[presidents_dolar['Time'].dt.year < 2003]
@@ -174,5 +158,5 @@ dilma_temer_euro = presidents_euro.copy(
        )[(presidents_euro['Time'].dt.year >= 2011) & (presidents_euro['Time'].dt.year < 2019)]
 bolso_euro = presidents_euro.copy(
        )[(presidents_euro['Time'].dt.year >= 2019) & (presidents_euro['Time'].dt.year <= 2021)]
-MOEDA_DOLAR = "EUR"
-visualyze_presidents(fhc_euro,lula_euro,dilma_temer_euro,bolso_euro,MOEDA_DOLAR)
+MOEDA_EURO = "EUR"
+visualyze_presidents(fhc_euro,lula_euro,dilma_temer_euro,bolso_euro,MOEDA_EURO)
